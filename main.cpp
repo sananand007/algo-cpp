9cdae63f321cefe7aa234fc2fc33a8b7d7024e61
Sandeep Anand <anandjairam@ufl.edu>
2017-06-21 23:42:31 -0700

    [6-21]merge : initial commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
 void insertionsort(int *array, int l);
+void insertionsort_noninc(int *array,int l);
+void merge(int *array,int p, int q, int r);
 int main(int argc, char *argv[]) {
-
-    int array[8];
-    cout<<"Enter 8 numbers:"<<endl;
-    for (int i = 0; i < 8; ++i) {
+    int array[6];
+    cout<<"Enter 6 numbers:"<<endl;
+    for (int i = 0; i < 6; ++i) {
         cin>>array[i];
     }
     cout<<endl;
 
     cout<<"Originally entered array is:"<<endl;
-    for (int j = 0; j < 8; ++j) {
+    for (int j = 0; j < 6; ++j) {
         cout<<array[j];
         cout<<endl;
     }
@@ -22,10 +22,12 @@ int main(int argc, char *argv[]) {
 
     cout<<"The length of the array is:"<<endl;
     cout<<l<<endl;
-
     insertionsort(array, l);
+    insertionsort_noninc(array,l);
+}
 
-    //now we do Insertion sorting
+// Merge(A,p,q,r)
+void merge(int *array, int p, int q, int r) {
 
 }
 
@@ -40,14 +42,25 @@ void insertionsort(int *array, int l) {
         }
         array[m+1]=key;
     }
-
     cout<<"Print the new array after the insertion sort"<<endl;
     for (int n = 0; n < l; ++n) {
-
         cout<<array[n]<<endl;
-
     }
-
-
 }
 
+void insertionsort_noninc(int *array, int l){
+
+    for (int i = 1; i < l; ++i) {
+        int keyval = array[i];
+        int j = i-1;
+        while(j>=0 && array[j]<keyval){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1]=keyval;
+    }
+    cout<<"Array in non Icreasing sort"<<endl;
+    for (int k = 0; k < l; ++k) {
+        cout<<array[k]<<endl;
+    }
+}
